5a70cabbeb8500ea25afd803eeb11c3d3c88e43e
lyqingye <>
2023-02-10 20:43:14 +0800

    feat: Iter modules

--- src/process/modules.rs
+++ src/process/modules.rs
@@ -25,4 +25,31 @@ impl<'a> ProcessModule<'a> {
             })?;
         Ok(module)
     }
+
+    pub fn get_main_module(&self) -> Result<Option<ModuleInfo>> {
+        let (peb, _) = self.ps.runtime().get_peb64(self.ps.handle())?;
+        let mut module = None;
+        self.ps
+            .runtime()
+            .enum_modules64(self.ps.handle(), &mut |module_info| {
+                if module_info.base_address == peb.ImageBaseAddress as usize {
+                    module = Some(module_info);
+                    true
+                } else {
+                    false
+                }
+            })?;
+        Ok(module)
+    }
+
+    pub fn modules(&self) -> Result<Vec<ModuleInfo>> {
+        let mut modules = Vec::new();
+        self.ps
+            .runtime()
+            .enum_modules64(self.ps.handle(), &mut |module_info| {
+                modules.push(module_info);
+                false
+            })?;
+        Ok(modules)
+    }
 }
